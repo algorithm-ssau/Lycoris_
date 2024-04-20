f56dd26e5cd5995b04c96c0a9b97758a818281d9
Timur Suleymanov <timur-sul86@mail.ru>
2024-04-20 22:12:57 +0400

    Add mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,17 @@
-
-// include 
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const { connectToDb, getDb } = require('./db');
 
-// select port
 const PORT = 3000;
 
-// start of server
 const app = express();
 
-// connect db
 let db;
 
-// console output
 connectToDb((err) => {
   if (!err) {
     app.listen(PORT, (err) => {
-      err ? console.log(err) : console.log(`Listening port ${PORT}`);
+      err ? console.log(err) : console.log(`listening port ${PORT}`);
     });
     db = getDb();
   } else {
@@ -24,21 +19,54 @@ connectToDb((err) => {
   }
 });
 
+const handleError = (res, error) => {
+  res.status(500).json({ error });
+}
+
 app.get('/movies', (req, res) => {
   const movies = [];
 
   db
     .collection('movies')
     .find()
+    .sort({ title: 1 })
     .forEach((movie) => movies.push(movie))
     .then(() => {
       res
         .status(200)
         .json(movies);
     })
-    .catch(() => {
+    .catch(() => handleError(res, "Something goes wrong..."));
+});
+
+app.get('/movies/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db
+    .collection('movies')
+    .findOne({ _id: new ObjectId(req.params.id) })
+    .then((doc) => {
       res
-        .status(500)
-        .json({ error: "Something goes wrong..." })
+        .status(200)
+        .json(doc);
     })
+    .catch(() => handleError(res, "Something goes wrong..."));
+  } else {
+    handleError(res, "Wrong id");
+  }
+});
+
+app.delete('/movies/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db
+    .collection('movies')
+    .deleteOne({ _id: new ObjectId(req.params.id) })
+    .then((result) => {
+      res
+        .status(200)
+        .json(result);
+    })
+    .catch(() => handleError(res, "Something goes wrong..."));
+  } else {
+    handleError(res, "Wrong id");
+  }
 });
