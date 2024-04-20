9e57fc5483bb1c87c05af3a544a7e7a6ae509b51
Timur Suleymanov <timur-sul86@mail.ru>
2024-04-21 02:51:49 +0400

    Add requests

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
 const Flower = require('./models/flower');
+const Order = require('./models/order');
+const User = require('./models/user');
 
 const PORT = 3000;
-const URL = "mongodb://localhost:27017/lycoris_";//"mongodb://localhost:27017/moviebox";
+const URL = "mongodb://localhost:27017/lycoris_";
 
 const app = express();
 app.use(express.json());
@@ -22,6 +24,8 @@ const handleError = (res, error) => {
   res.status(500).json({ error });
 }
 
+// flower request
+{
 app.get('/flower', (req, res) => {
   Flower
     .find()
@@ -78,3 +82,124 @@ app.patch('/flower/:id', (req, res) => {
   })
   .catch(() => handleError(res, "Something goes wrong..."));
 });
+}
+
+// order request
+{
+  app.get('/order', (req, res) => {
+    Order
+      .find()
+      .sort({ title: 1 })
+      .then((result) => {
+        res
+          .status(200)
+          .json(result);
+      })
+      .catch(() => handleError(res, "Something goes wrong..."));
+  });
+  
+  app.get('/order/:id', (req, res) => {
+    Order
+    .findById(req.params.id)
+    .then((result) => {
+      res
+        .status(200)
+        .json(result);
+    })
+    .catch(() => handleError(res, "Something goes wrong..."));
+  });
+  
+  app.delete('/order/:id', (req, res) => {
+    Order
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res
+        .status(200)
+        .json(result);
+    })
+    .catch(() => handleError(res, "Something goes wrong..."));
+  });
+  
+  app.post('/order', (req, res) => {
+    const flower = new Order(req.body);
+    flower
+    .save()
+    .then((result) => {
+      res
+        .status(201)
+        .json(result);
+    })
+    .catch(() => handleError(res, "Something goes wrong..."));
+  });
+  
+  app.patch('/order/:id', (req, res) => {
+    Order
+    .findByIdAndUpdate(req.params.id, req.body)
+    .then((result) => {
+      res
+        .status(200)
+        .json(result);
+    })
+    .catch(() => handleError(res, "Something goes wrong..."));
+  });
+}
+
+// user request
+{
+  app.get('/user', (req, res) => {
+    User
+      .find()
+      .sort({ title: 1 })
+      .then((result) => {
+        res
+          .status(200)
+          .json(result);
+      })
+      .catch(() => handleError(res, "Something goes wrong..."));
+  });
+  
+  app.get('/user/:id', (req, res) => {
+    User
+    .findById(req.params.id)
+    .then((result) => {
+      res
+        .status(200)
+        .json(result);
+    })
+    .catch(() => handleError(res, "Something goes wrong..."));
+  });
+  
+  app.delete('/user/:id', (req, res) => {
+    User
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res
+        .status(200)
+        .json(result);
+    })
+    .catch(() => handleError(res, "Something goes wrong..."));
+  });
+  
+  app.post('/user', (req, res) => {
+    const flower = new User(req.body);
+    flower
+    .save()
+    .then((result) => {
+      res
+        .status(201)
+        .json(result);
+    })
+    .catch(() => handleError(res, "Something goes wrong..."));
+  });
+  
+  app.patch('/user/:id', (req, res) => {
+    User
+    .findByIdAndUpdate(req.params.id, req.body)
+    .then((result) => {
+      res
+        .status(200)
+        .json(result);
+    })
+    .catch(() => handleError(res, "Something goes wrong..."));
+  });
+}
