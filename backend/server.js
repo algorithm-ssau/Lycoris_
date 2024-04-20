599331966e244f2baad29d5aa1c570be3e320a78
Timur Suleymanov <timur-sul86@mail.ru>
2024-04-20 15:12:39 +0400

    Add request

--- backend/server.js
+++ backend/server.js
@@ -23,3 +23,22 @@ connectToDb((err) => {
     console.log(`DB connection error: ${err}`);
   }
 });
+
+app.get('/movies', (req, res) => {
+  const movies = [];
+
+  db
+    .collection('movies')
+    .find()
+    .forEach((movie) => movies.push(movie))
+    .then(() => {
+      res
+        .status(200)
+        .json(movies);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "Something goes wrong..." })
+    })
+});
